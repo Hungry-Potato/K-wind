ab105d81bd75629848c7c85ae6cceda5504ebe8c
laewonJeong <laewonjeong@kangwon.ac.kr>
2024-12-04 16:52:07 +0900

    add get_match_data

--- client_test.py
+++ client_test.py
@@ -2,7 +2,7 @@ import requests
 import json
 from urllib import parse
 
-API_KEY = 'RGAPI-9805a7c6-6f36-4a2f-8a92-bf7e2ae23d7b'
+API_KEY = ''
 REQUEST_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -26,6 +26,24 @@ def get_puuid(user_nickname, tag_line):
             print(f"Error fetching data: {e}")
             return False
 
+def get_match_data(puuid):
+    url = f"https://kr.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/{puuid}"
+    
+    while True:
+        try:
+            response = requests.get(url, headers=REQUEST_HEADERS)
+            response.raise_for_status()
+            match_data = response.json()
+        
+            if match_data['gameMode'] == 'ARAM':
+                return match_data
+                
+            return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data: {e}")
+            return False
+
+
 #main
 if __name__ == "__main__":
     nickname = input('Input Nickname: ')
@@ -33,7 +51,8 @@ if __name__ == "__main__":
     
     puuid = get_puuid(nickname, tag_line)
     
-    if not puuid:
-        print('Error')
+    data = get_match_data(puuid)
+    if not data:
+        print("진행중인 게임이 칼바람나락이 아닙니다.")
     else:
-        print(puuid)
+        print(data)
